68ba0f0089746ba1437b1e905c286b9aa13b39de
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2023-03-06 09:34:49 +0700

    Update

---
+++ Resources/views/mail/contact.blade.php
@@ -0,0 +1,4 @@
+<p style="margin-bottom: 12px">Guest name: <b>{{$name}}</b></p>
+<p style="margin-bottom: 12px">Guest email:<b> {{$email}}</b></p>
+<p style="margin-bottom: 12px">Subject: <b>{{$subject}}</b></p>
+<p style="margin-bottom: 12px">Content: <b>{{$content}}</b></p>
